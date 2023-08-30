9c95dc024d5fda24eb2c667a96dcad2aae94398d
Matthew Smallhouse <smallhou@bc.edu>
2023-08-30 18:41:45 -0400

    added playGame module so that clicks alternate between player1 and player2

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+const personFactory = (marker) => {
+    const placeMarker = (element) => {
+        element.textContent = marker;
+    };
+
+    return { placeMarker, marker };
+};
+const player1 = personFactory("X");
+const player2 = personFactory("O");
+
+const playGame = (() => {
+    this.player = player1;
+
+    return { player };
+})();
+
 const gameboard = (() => {
     this.tiles = document.getElementsByClassName("grid-box");
-    for (let i=0; i<9; i++) {
-        tiles[i].textContent = "O";
-    }
+    Array.from(tiles).forEach((element) => {
+        element.addEventListener("click", () => {
+            element.textContent = playGame.player.marker;
+
+            if (playGame.player === player1) {
+                playGame.player = player2;
+            }
+            else {
+                playGame.player = player1;
+            }
+        });
+    });
 })();
